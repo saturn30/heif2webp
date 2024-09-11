00227bab721348e029155331b03f94a2bed5d924
saturn30 <soso01dev@gmail.com>
2024-09-11 22:45:34 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import * as fs from "node:fs";
+import * as os from "node:os";
+import * as path from "node:path";
+import sharp from "sharp";
+
+async function main() {
+  const files = getDownloadFolderFiles();
+  const hiefFiles = files.filter((name) => name.endsWith(".HIF"));
+  for (const path of hiefFiles) {
+    await sharp(path)
+      .webp({ quality: 100 })
+      .toFile(path.replace(".HIF", ".webp"));
+  }
+}
+
+function getDownloadFolderFiles(): string[] {
+  const homeDir = os.homedir();
+  const downloadDir = path.join(homeDir, "Downloads");
+  try {
+    const files = fs.readdirSync(downloadDir);
+    return files.map((fileName) => `${downloadDir}/${fileName}`);
+  } catch (error) {
+    console.error("Error reading download directory:", error);
+    return [];
+  }
+}
+
+main();
